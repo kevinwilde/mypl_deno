4e3dbd8c881e3830e944c63c89e9c40feaad9467
Kevin Wilde <kevin.wilde@coursehero.com>
2020-06-01 00:09:53 -0700

    add lots of tests and pretty printer for types

--- test/tests.ts
+++ test/tests.ts
@@ -1,7 +1,7 @@
 import { createLexer } from "../lexer.ts";
 import { createAST } from "../parser.ts";
 import { evaluate } from "../interpreter.ts";
-import { prettyPrint } from "../utils.ts";
+import { prettyPrint, printType } from "../utils.ts";
 import { typeCheck } from "../typechecker.ts";
 
 function assertResult(
@@ -42,6 +42,25 @@ function assertResult(
   }
 }
 
+function assertType(
+  program: string,
+  expectedType: string,
+) {
+  const lexer = createLexer(program);
+  const ast = createAST(lexer);
+  const actualType = printType(typeCheck(ast));
+  const success = actualType === expectedType;
+  if (!success) {
+    console.log("AST:");
+    console.log(prettyPrint(ast));
+    console.log("Actual type:");
+    console.log(actualType);
+    console.log("Expected type:");
+    console.log(expectedType);
+    throw new Error("Test failed");
+  }
+}
+
 function expectTypeError(program: string) {
   const lexer = createLexer(program);
   const ast = createAST(lexer);
@@ -60,21 +79,25 @@ function expectTypeError(program: string) {
 
 Deno.test("defining a variable (int)", () => {
   let program = "(let x 1 x)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
 });
 
 Deno.test("defining a variable (string)", () => {
   let program = `(let x "hello" x)`;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "hello" });
 });
 
 Deno.test("calling a function (with type ann)", () => {
   let program = "((lambda (x:bool) x) #t)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: true });
 });
 
 Deno.test("calling a function (without type ann)", () => {
   let program = "((lambda (x) x) #t)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: true });
 });
 
@@ -85,15 +108,19 @@ Deno.test("[TypeError] calling a function", () => {
 
 Deno.test("calling a function with multiple args (with type ann)", () => {
   let program = " ((lambda (x:bool y:bool) x) #t #f)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: true });
   program = " ((lambda (x:bool y:bool) y) #t #f)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: false });
 });
 
 Deno.test("calling a function with multiple args (without type ann)", () => {
   let program = " ((lambda (x y) x) #t #f)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: true });
   program = " ((lambda (x y) y) #t #f)";
+  assertType(program, "bool");
   assertResult(program, { tag: "TmBool", val: false });
 });
 
@@ -106,15 +133,19 @@ Deno.test("[TypeError] calling a function with multiple args", () => {
 
 Deno.test("conditionals (with type ann)", () => {
   let program = "((lambda (x:bool y:int z:int) (if x y z)) #t 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
   program = "((lambda (x:bool y:int z:int) (if x y z)) #f 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 2 });
 });
 
 Deno.test("conditionals (wthout type ann)", () => {
   let program = "((lambda (x y z) (if x y z)) #t 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
   program = "((lambda (x y z) (if x y z)) #f 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 2 });
 });
 
@@ -134,12 +165,19 @@ Deno.test("[TypeError] conditionals (without type ann)", () => {
 
 Deno.test("addition (with type ann)", () => {
   let program = " (let plus (lambda (x: int y :int) (+ x y)) (plus 2 3))";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 5 });
 });
 
 Deno.test("addition (without type ann)", () => {
   let program = " (let plus (lambda (x y) (+ x y)) (plus 2 3))";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 5 });
+});
+
+Deno.test("plus fn (without type ann)", () => {
+  let program = " (let plus (lambda (x y) (+ x y)) plus)";
+  assertType(program, "(-> (int int) int)");
 });
 
 Deno.test("[TypeError] stdlib function", () => {
@@ -165,31 +203,38 @@ Deno.test("[TypeError] using stdlib function (without type ann)", () => {
 
 Deno.test("subtraction (with type ann)", () => {
   let program = " (let minus (lambda (x:int y:int) (- x y)) (minus 2 3))";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: -1 });
 });
 
 Deno.test("subtraction (without type ann)", () => {
   let program = " (let minus (lambda (x y) (- x y)) (minus 2 3))";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: -1 });
 });
 
 Deno.test("conditional with equality (with type ann)", () => {
   let program =
     "((lambda (w:int x:int y:int z:int) (if (= w x) y z)) 42 42 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
   program = "((lambda (w:int x:int y:int z:int) (if (= w x) y z)) 42 43 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 2 });
 });
 
 Deno.test("conditional with equality (without type ann)", () => {
   let program = "((lambda (w x y z) (if (= w x) y z)) 42 42 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
   program = "((lambda (w x y z) (if (= w x) y z)) 42 43 1 2)";
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 2 });
 });
 
 Deno.test("string-concat", () => {
   let program = `(let x (string-concat "hello" "world") x)`;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "helloworld" });
 });
 
@@ -201,6 +246,7 @@ Deno.test("closure (with type ann)", () => {
          (addN 1)
          (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
 });
 
@@ -212,6 +258,7 @@ Deno.test("closure (without type ann)", () => {
          (addN 1)
          (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
 });
 
@@ -275,6 +322,7 @@ Deno.test("closure not fooled by later shadow - maintains env where defined (wit
       (let x 1 (lambda (y:int) (+ x y)))
       (let x 2 (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
   // Even when shadow changes the type
   program = `
@@ -282,6 +330,7 @@ Deno.test("closure not fooled by later shadow - maintains env where defined (wit
       (let x 1 (lambda (y:int) (+ x y)))
       (let x "hi" (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
 });
 
@@ -291,6 +340,7 @@ Deno.test("closure not fooled by later shadow - maintains env where defined (wit
       (let x 1 (lambda (y) (+ x y)))
       (let x 2 (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
   // Even when shadow changes the type
   program = `
@@ -298,6 +348,7 @@ Deno.test("closure not fooled by later shadow - maintains env where defined (wit
       (let x 1 (lambda (y) (+ x y)))
       (let x "hi" (add1 42)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 43 });
 });
 
@@ -307,6 +358,7 @@ Deno.test("shadowing (with type ann)", () => {
       (let x 1 (let x 2 (lambda (y:int) (+ x y))))
       (add2 42))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 44 });
   // Even when shadow changes the type
   program = `
@@ -314,6 +366,7 @@ Deno.test("shadowing (with type ann)", () => {
       (let x "hi" (let x 2 (lambda (y:int) (+ x y))))
       (add2 42))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 44 });
 });
 
@@ -323,6 +376,7 @@ Deno.test("shadowing (without type ann)", () => {
       (let x 1 (let x 2 (lambda (y) (+ x y))))
       (add2 42))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 44 });
   // Even when shadow changes the type
   program = `
@@ -330,6 +384,7 @@ Deno.test("shadowing (without type ann)", () => {
       (let x "hi" (let x 2 (lambda (y) (+ x y))))
       (add2 42))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 44 });
 });
 
@@ -341,6 +396,7 @@ Deno.test("first class functions (with type ann)", () => {
           (lambda (x:int) (+ x 1))
           (doTwice add1 5)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 7 });
 });
 
@@ -352,6 +408,7 @@ Deno.test("first class functions (without type ann)", () => {
           (lambda (x) (+ x 1))
           (doTwice add1 5)))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 7 });
 });
 
@@ -392,6 +449,7 @@ Deno.test("[TypeError] first class functions (without type ann)", () => {
           (lambda (x) (string-concat x "world"))
           (doTwice add1 "hi")))
   `;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "hiworldworld" });
 });
 
@@ -401,12 +459,14 @@ Deno.test("first class function with stdlib (with type ann)", () => {
       (lambda (f:(-> (int int) int) x:int y:int) (f x (f x y)))
       (doTwice + 5 8))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 18 });
   program = `
   (let doTwice
       (lambda (f:(-> (str str) str) x:str y:str) (f x (f x y)))
       (doTwice string-concat "Be" " Rhexa"))
   `;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "BeBe Rhexa" });
 });
 
@@ -416,12 +476,14 @@ Deno.test("first class function with stdlib (without type ann)", () => {
       (lambda (f x y) (f x (f x y)))
       (doTwice + 5 8))
   `;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 18 });
   program = `
   (let doTwice
       (lambda (f x y) (f x (f x y)))
       (doTwice string-concat "Be" " Rhexa"))
   `;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "BeBe Rhexa" });
 });
 
@@ -508,6 +570,7 @@ Deno.test("naive factorial (without type ann)", () => {
           (* n (fct (- n 1))))))
   `;
   let program = `(let factorial (fix ${g}) (factorial 0))`;
+  assertType(program, "int");
   assertResult(program, { tag: "TmInt", val: 1 });
   program = `(let factorial (fix ${g}) (factorial 1))`;
   assertResult(program, { tag: "TmInt", val: 1 });
@@ -530,6 +593,7 @@ Deno.test("recursive function that takes int returns str", () => {
           (string-concat "a" (append-n (- n 1))))))
   `;
   let program = `(let myFunc (fix ${g}) (myFunc 0))`;
+  assertType(program, "str");
   assertResult(program, { tag: "TmStr", val: "" });
   program = `(let myFunc (fix ${g}) (myFunc 1))`;
   assertResult(program, { tag: "TmStr", val: "a" });
@@ -569,3 +633,30 @@ Deno.test("recursive function that takes int returns str", () => {
 //   program = `(let fibonacci (${fix} ${g}) (fibonacci 5))`;
 //   assertResult(program, { tag: "TmInt", val: 8 });
 // });
+
+Deno.test("identity fn", () => {
+  let program = "(lambda (x) x)";
+  assertType(program, "(-> ('a) 'a)");
+});
+
+Deno.test("type inference on function", () => {
+  let program = "(lambda (x y) (if x 0 1))";
+  assertType(program, "(-> (bool 'a) int)");
+});
+
+Deno.test("type inference on functions with free types", () => {
+  let program = "(lambda (x y) (if x y y))";
+  assertType(program, "(-> (bool 'a) 'a)");
+  program = "(lambda (x y z) (if x y z))";
+  assertType(program, "(-> (bool 'a 'a) 'a)");
+  program = "(lambda (x y z) (if x y y))";
+  assertType(program, "(-> (bool 'a 'b) 'a)");
+  program = "(lambda (x y z) (if x z z))";
+  assertType(program, "(-> (bool 'a 'b) 'b)");
+  program =
+    "(lambda (x y z1 z2 z3 z4 z5 z6 z7 z8 z9 z10 z11 z12 z13 z14 z15) (if x 0 y))";
+  assertType(
+    program,
+    "(-> (bool int 'a 'b 'c 'd 'e 'f 'g 'h 'i 'j 'k 'l 'm 'n 'o) int)",
+  );
+});
--- utils.ts
+++ utils.ts
@@ -1,3 +1,62 @@
+import { typeCheck } from "./typechecker.ts";
+
 export function prettyPrint(obj: any) {
   return JSON.stringify(obj, null, 2);
+}
+
+export function printType(t: ReturnType<typeof typeCheck>) {
+  function helper(
+    t: ReturnType<typeof typeCheck>,
+  ): string {
+    switch (t.tag) {
+      case "TyBool":
+        return "bool";
+      case "TyInt":
+        return "int";
+      case "TyStr":
+        return "str";
+      case "TyArrow":
+        return `(-> (${(t.paramTypes.map((p) => helper(p))).join(" ")}) ${
+          helper(t.returnType)
+        })`;
+      case "TyId":
+        return `%${t.name}%`;
+      default: {
+        const _exhaustiveCheck: never = t;
+        throw new Error();
+      }
+    }
+  }
+
+  // produces stream of identifiers like
+  // 'a 'b 'c ... 'z 'aa 'ab 'ac ... 'az 'ba 'bb 'bc ... 'bz 'ca 'cb 'cc ...
+  const nextFreeGenerator = () => {
+    let i = 0;
+    return () => {
+      let n = i;
+      let result = "'";
+      while (n >= 26) {
+        const multiple = Math.floor(n / 26);
+        result += String.fromCharCode(97 + multiple - 1);
+        n -= (26 * multiple);
+      }
+      result += String.fromCharCode(97 + (n % 26));
+      i += 1;
+      return result;
+    };
+  };
+
+  const uglyResult = helper(t);
+  const regex = new RegExp("(\%.+?\%)");
+  const nextFree = nextFreeGenerator();
+  let result = uglyResult;
+  while (true) {
+    const matches = regex.exec(result);
+    if (matches === null) {
+      break;
+    }
+    const match = matches[0];
+    result = result.split(match).join(nextFree());
+  }
+  return result;
 }
